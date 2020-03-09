9ac8790b6251cac41d27a7d77513cbff48fdd728
tonare <carlos.renato.simoes@gmail.com>
2020-03-09 17:39:10 -0300

    Infra data layer new classes.

--- CleanArch/ClearArch.Mvc/Startup.cs
+++ CleanArch/ClearArch.Mvc/Startup.cs
@@ -12,6 +12,7 @@ using ClearArch.Mvc.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using CleanArch.Infra.Data.Context;
 
 namespace ClearArch.Mvc
 {
@@ -32,6 +33,12 @@ namespace ClearArch.Mvc
                     Configuration.GetConnectionString("UniversityIdentityDBConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddDbContext<UniversityDBContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("UniversityDBConnection"));
+            });
+
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
