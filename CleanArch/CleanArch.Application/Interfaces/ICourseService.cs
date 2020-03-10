a0cf7815bd1b051f81f799728198c94c364e1a89
tonare <carlos.renato.simoes@gmail.com>
2020-03-10 10:05:31 -0300

    First service.

---
+++ CleanArch/CleanArch.Application/Interfaces/ICourseService.cs
@@ -0,0 +1,13 @@
+﻿using CleanArch.Application.ViewModels;
+using CleanArch.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Application.Interfaces
+{
+    public interface ICourseService
+    {
+        IEnumerable<CourseViewModel> GetCourses();
+    }
+}
