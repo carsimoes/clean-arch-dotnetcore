1ec276a190fc47418f35c9a4aab1197cbd69e8f0
tonare <carlos.renato.simoes@gmail.com>
2020-03-10 14:52:13 -0300

    add commands handlers and updated course repository

--- CleanArch/CleanArch.Domain/Interfaces/ICourseRepository.cs
+++ CleanArch/CleanArch.Domain/Interfaces/ICourseRepository.cs
@@ -8,5 +8,7 @@ namespace CleanArch.Domain.Interfaces
     public interface ICourseRepository
     {
         IEnumerable<Course> GetCourses();
+
+        void Add(Course course);
     }
 }
--- CleanArch/CleanArch.Infra.IoC/DependecyContainer.cs
+++ CleanArch/CleanArch.Infra.IoC/DependecyContainer.cs
@@ -1,7 +1,13 @@
 ﻿using CleanArch.Application.Interfaces;
 using CleanArch.Application.Services;
+using CleanArch.Domain.CommandHandlers;
+using CleanArch.Domain.Commands;
+using CleanArch.Domain.Core.Bus;
 using CleanArch.Domain.Interfaces;
+using CleanArch.Infra.Bus;
+using CleanArch.Infra.Data.Context;
 using CleanArch.Infra.Data.Repository;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CleanArch.Infra.IoC
@@ -10,12 +16,18 @@ namespace CleanArch.Infra.IoC
     {
         public static void RegisterServices(IServiceCollection services)
         {
+            //Domain InMemoryBus MediatR
+            services.AddScoped<IMediatorHandler, InMemoryBus>();
+
+            //Domain Handlers
+            services.AddScoped<IRequestHandler<CreateCourseCommand, bool>, CourseCommandHandler>();
+
             //Application layer
             services.AddScoped<ICourseService, CourseService>();
 
             //Infra.Data layer
             services.AddScoped<ICourseRepository, CourseRepository>();
-
+            services.AddScoped<UniversityDBContext>();
 
         }
     }
